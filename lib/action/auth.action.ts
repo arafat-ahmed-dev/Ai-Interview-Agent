89c693d723b9f57af8f4497c3fea5ab08d2954b0
arafat-ahmed-dev <arafatahmedshlbd194@gmail.com>
2025-03-25 03:24:23 +0600

    add the hole logic for sign-in and sign-up and connect to firebase authentication

---
+++ lib/action/auth.action.ts
@@ -0,0 +1,71 @@
+"use server";
+import { auth, db } from "@/firebase/admin";
+import { cookies } from "next/headers";
+
+export async function signUp(params: SignUpParams) {
+  const { uid, name, email } = params;
+  try {
+    const userRecord = await db.collection("users").doc(uid).get();
+    if (userRecord.exists) {
+      return {
+        success: false,
+        message: "User already exists. Please sign in instead.",
+      };
+    }
+    await db.collection("users").doc(uid).set({
+      name,
+      email,
+    });
+    return {
+      success: true,
+      message: "Account signed up successfully. Please sign in!",
+    };
+  } catch (e: any) {
+    console.error("Error creating a user", e);
+    if (e.code === "auth/email-already-exists") {
+      return {
+        success: false,
+        message:
+          "Already used with that email. Please use a different email address.",
+      };
+    }
+    return {
+      success: false,
+      message: "Failed to create a user",
+    };
+  }
+}
+
+export async function signIn(params: SignInParams) {
+  const { email, idToken } = params;
+  try {
+    const userRecord = await auth.getUserByEmail(email);
+    if (!userRecord) {
+      return {
+        success: false,
+        message: "User does not exist. Please create a account instead.",
+      };
+    }
+    await setSessionCookie(idToken);
+  } catch (e) {
+    console.error("Error logging a user", e);
+    return {
+      success: false,
+      message: "Failed to log into an account.",
+    };
+  }
+}
+
+export async function setSessionCookie(idToken: string) {
+  const cookieStore = await cookies();
+  const ONE_WEEK = 60 * 60 * 24 * 7;
+  const sessionCookie = await auth.createSessionCookie(idToken, {
+    expiresIn: ONE_WEEK * 1000,
+  });
+  cookieStore.set("session", sessionCookie, {
+    maxAge: ONE_WEEK,
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
+  });
+}
